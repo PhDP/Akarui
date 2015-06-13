60b86278cbc911bb08366adaff48a4405f46cdb8
Philippe Desjardins-Proulx <philippe.d.proulx@gmail.com>
2015-06-13 19:41:18 -0400

    it compiles!

--- tests/Properties.hs
+++ tests/Properties.hs
@@ -1,9 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Test.QuickCheck
-import Data.List
-import Sphinx.Formula
-import Sphinx.FOL
+-- import Data.List
+-- import Sphinx.Formula
+-- import Sphinx.FOL
+
+--tree = sized tree'
+--tree' 0 = liftM Leaf arbitrary
+--tree' n | n>0 =
+--  oneof [liftM Leaf arbitrary,
+--           liftM2 Branch subtree subtree]
+--             where subtree = tree' (n `div` 2)
 
 qsort :: Ord a => [a] -> [a]
 qsort [] = []
@@ -12,15 +19,12 @@ qsort (x:xs) = qsort lhs ++ [x] ++ qsort rhs
     lhs = filter (< x) xs
     rhs = filter (>= x) xs
 
+prop_ordered :: [Int] -> Bool
 prop_ordered xs = ordered (qsort xs)
  where
    ordered [] = True
-   ordered [x] = True
-   ordered (x:y:xs) = x <= y && ordered (y:xs)
+   ordered [_] = True
+   ordered (x:y:_) = x <= y && ordered (y:xs)
 
---tree = sized tree'
---tree' 0 = liftM Leaf arbitrary
---tree' n | n>0 =
---  oneof [liftM Leaf arbitrary,
---           liftM2 Branch subtree subtree]
---             where subtree = tree' (n `div` 2)
+main :: IO ()
+main = quickCheck prop_ordered

3a2bb1964999d2858e4c2cc328df8aeda6511e19
Philippe Desjardins-Proulx <philippe.d.proulx@gmail.com>
2015-06-23 00:44:27 -0400

    cleaning maxWalkSAT, use sets intead of lists

--- Sphinx/WalkSAT.hs
+++ Sphinx/WalkSAT.hs
@@ -4,8 +4,6 @@ module Sphinx.WalkSAT where
 import Data.Map (Map)
 import qualified Data.Map as Map
 import qualified Data.Set as Set
-import Data.List (minimumBy)
-import Data.Function (on)
 import System.Random
 import Sphinx.Formula
 
@@ -16,7 +14,6 @@ import Sphinx.Formula
 -- Reference:
 --   P Domingos and D Lowd, Markov Logic: An Interface Layer for Artificial
 -- Intelligence, 2009, Morgan & Claypool. p. 24.
--- WalkSAT :: (Ord a) => Int -> Int -> Double -> Double -> Int -> [(Formula a, Double)] -> Maybe (Map a Bool)
 maxWalkSAT :: Ord a => Int -> Int -> Double -> Double -> Int -> Map (Formula a) Double -> Maybe (Map a Bool)
 maxWalkSAT mt mf target p seed fs = step (mkStdGen seed) mt
   where
@@ -31,7 +28,7 @@ maxWalkSAT mt mf target p seed fs = step (mkStdGen seed) mt
         (seed0, r') = random r :: (Int, StdGen)
         (seed1, _) = random r' :: (Int, StdGen)
       in
-        case flips r (randomFairAss (mkStdGen seed0) vars) mf of
+        case flipStep r (randomFairAss (mkStdGen seed0) vars) mf of
           Just ass -> Just ass
           Nothing  -> step (mkStdGen seed1) (n - 1) -- Try again...
 
@@ -42,32 +39,28 @@ maxWalkSAT mt mf target p seed fs = step (mkStdGen seed) mt
         cost' = Map.fold (+) 0.0 unsatisfied'
 
     -- The 'flips' steps take a rng, a solution, and the number of flips left:
-    flips _ _ 0   = Nothing
-    flips r s n = if cost <= target then Just s else flips r''' s' (n - 1)
+    flipStep _ _ 0   = Nothing
+    flipStep r s n = if cost <= target then Just s else flipStep r''' s' (n - 1)
       where
         -- List of unsatisfied formula under soln:
         unsatisfied = Map.filterWithKey (\k _ -> unsatisfiable s k) fs
-        -- Number of unsatisfied formulas:
-        numUn = Map.size unsatisfied
         -- Sum of weights of unsatisfied clauses in soln
         cost = Map.fold (+) 0.0 unsatisfied
 
         -- Pick a clause randomly among the unsatisfied clauses:
-        (idx0, r') = randomR (0, numUn - 1) r :: (Int, StdGen)
+        (idx0, r') = randomR (0, Map.size unsatisfied - 1) r :: (Int, StdGen)
         (c, _) = Map.elemAt idx0 unsatisfied
 
-        vs = atomsLs c
+        -- Maps deltaCosts to atoms in the selected clause c:
+        da = Map.fromList $ map (\a -> (deltaCost cost s a, a)) (atomsLs c)
 
-        vlow = fst $
-          minimumBy (compare `on` snd)
-            $ map (\v' -> (v', deltaCost cost s v')) vs
-
+        -- Whether a random atom is flipped or the one with the lowest deltaCost flips.
         (flipTest, r'') = random r' :: (Double, StdGen)
-        isFlipping = flipTest < p
+        flips = flipTest < p
 
         -- Pick an atom randomly in the randomly selected clause:
-        (idx1, r''') = randomR (0, length vs - 1) r'' :: (Int, StdGen)
-        vf = if isFlipping then vs !! idx1 else vlow
+        (idx1, r''') = randomR (0, Map.size da - 1) r'' :: (Int, StdGen)
+        a' = snd $ if flips then Map.elemAt idx1 da else Map.findMin da
 
         -- Solution with vf flipped
-        s' = Map.adjust not vf s
+        s' = Map.adjust not a' s

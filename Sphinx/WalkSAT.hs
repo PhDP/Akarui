0458322c1113954aeb02ea04950435cca3d755ef
Philippe Desjardins-Proulx <philippe.d.proulx@gmail.com>
2015-06-23 00:28:10 -0400

    use maps instead of lists for maxWalkSAT

--- Sphinx/WalkSAT.hs
+++ Sphinx/WalkSAT.hs
@@ -3,12 +3,13 @@ module Sphinx.WalkSAT where
 
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Data.List (foldl', minimumBy)
+import qualified Data.Set as Set
+import Data.List (minimumBy)
 import Data.Function (on)
 import System.Random
 import Sphinx.Predicate
 import Sphinx.Formula
-import Sphinx.MLN
+import Sphinx.FOL
 
 -- | The MaxWalkSAT algorithm with a max number of tries (mt), max number
 -- of flips (mt), a target cost, a probability of flipping, and a markov
@@ -18,11 +19,11 @@ import Sphinx.MLN
 --   P Domingos and D Lowd, Markov Logic: An Interface Layer for Artificial
 -- Intelligence, 2009, Morgan & Claypool. p. 24.
 -- WalkSAT :: (Ord a) => Int -> Int -> Double -> Double -> Int -> [(Formula a, Double)] -> Maybe (Map a Bool)
-maxWalkSAT :: Int -> Int -> Double -> Double -> Int -> MLN String -> Maybe (Map (Predicate String) Bool)
-maxWalkSAT mt mf target p seed mln = step (mkStdGen seed) mt
+maxWalkSAT :: Int -> Int -> Double -> Double -> Int -> Map (FOL String) Double -> Maybe (Map (Predicate String) Bool)
+maxWalkSAT mt mf target p seed fs = step (mkStdGen seed) mt
   where
     -- Set of atoms (predicates) in the MLN:
-    vars = predicates mln
+    vars = Set.unions $ map atoms (Map.keys fs)
 
     -- A single step. Returns a Maybe type with Nothing if no assignment with
     -- a cost lower than the target has been found.
@@ -39,27 +40,29 @@ maxWalkSAT mt mf target p seed mln = step (mkStdGen seed) mt
     -- Cost of flipping atom v
     deltaCost cost s v = cost' - cost
       where
-        unsatisfied' = filter (unsatisfiable (Map.adjust not v s) . fst) mln
-        cost' = foldl' (\acc f -> acc + snd f) 0.0 unsatisfied'
+        unsatisfied' = Map.filterWithKey (\k _ -> unsatisfiable (Map.adjust not v s) k) fs
+        cost' = Map.fold (+) 0.0 unsatisfied'
 
     -- The 'flips' steps take a rng, a solution, and the number of flips left:
     flips _ _ 0   = Nothing
     flips r s n = if cost <= target then Just s else flips r''' s' (n - 1)
       where
         -- List of unsatisfied formula under soln:
-        unsatisfied = filter (unsatisfiable s . fst) mln
+        unsatisfied = Map.filterWithKey (\k _ -> unsatisfiable s k) fs
+        -- Number of unsatisfied formulas:
+        numUn = Map.size unsatisfied
         -- Sum of weights of unsatisfied clauses in soln
-        cost = foldl' (\acc f -> acc + snd f) 0.0 unsatisfied
+        cost = Map.fold (+) 0.0 unsatisfied
 
         -- Pick a clause randomly among the unsatisfied clauses:
-        (idx0, r') = randomR (0, length unsatisfied - 1) r :: (Int, StdGen)
-        (c, _) = unsatisfied !! idx0
+        (idx0, r') = randomR (0, numUn - 1) r :: (Int, StdGen)
+        (c, _) = Map.elemAt idx0 unsatisfied
 
         vs = atomsLs c
 
         vlow = fst $
           minimumBy (compare `on` snd)
-                    (map (\v' -> (v', deltaCost cost s v')) vs)
+            $ map (\v' -> (v', deltaCost cost s v')) vs
 
         (flipTest, r'') = random r' :: (Double, StdGen)
         isFlipping = flipTest < p

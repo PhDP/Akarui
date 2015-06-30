5e97cdb32a5b2e772c71e9702bbc3dfb0bdbdb79
Philippe Desjardins-Proulx <philippe.d.proulx@gmail.com>
2015-06-30 01:15:46 -0400

    add function to format to GraphML

--- Manticore/Fmt.hs
+++ Manticore/Fmt.hs
@@ -16,6 +16,22 @@ fmtSet s = addBrackets $ drop 2 $ Set.foldl' (\acc k -> acc ++ ", " ++ show k) "
 fmtMapSet :: (Show k0, Show k1, Ord k0, Ord k1) => Map k0 (Set k1) -> String
 fmtMapSet = Map.foldWithKey (\k v acc -> show k ++ " -> " ++ fmtSet v ++ "\n" ++ acc) ""
 
+-- | Formats a map of sets in GraphML format.
+fmtGraphML :: (Show k0, Show k1, Ord k0, Ord k1) => String -> Map k0 (Set k1) -> String
+fmtGraphML name m = hd ++ gr ++ nodes ++ allEdges ++ end
+  where
+    hd = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" ++
+          "<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\"\n" ++
+          " xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" ++
+          " xsi:schemaLocation=\"http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd\">\n"
+    gr = "  <graph id=\"" ++ name ++ "\" edgedefault=\"directed\">\n"
+    nodes = Map.foldWithKey (\k _ acc -> "    <node id=\"" ++ show' k ++ "\"/>\n" ++ acc) "" m
+    allEdges = Map.foldWithKey (\k v acc -> edges k v ++ acc) "" m
+    edges from = Set.fold (\to acc -> "    <edge source=\"" ++ show' from ++ "\" target=\"" ++ show' to ++ "\"/>\n" ++ acc) ""
+    end = "  </graph>\n</graphml>\n"
+    -- To prevent issues with show String.
+    show' s = rmQuotes $ show s
+
 -- | Formats a map of maps (often used to represent networks.)
 fmtMapMap :: (Show k0, Show k1, Show v, Ord k0, Ord k1) => Map k0 (Map k1 v) -> String
 fmtMapMap = Map.foldWithKey vertices ""

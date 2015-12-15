ae4fe1d1f94f5587c107b7a0291cc17e99001c36
Philippe Desjardins-Proulx <philippe.d.proulx@gmail.com>
2015-12-14 23:08:18 -0500

    rm a few old files, up Fmt with Data.Text

--- Faun/Fmt.hs
+++ Faun/Fmt.hs
@@ -2,43 +2,46 @@
 -- is mostly convenient for playing with Sphinx in the console.
 module Faun.Fmt where
 
+import qualified Data.Text as T
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Faun.Text
+import Faun.ShowTxt
 
 -- | Formats a set in the standard format (not a 'fromList').
-fmtSet :: (Show k, Ord k) => Set k -> String
-fmtSet s = addBrackets $ drop 2 $ Set.foldl' (\acc k -> acc ++ ", " ++ show k) "" s
+fmtSet :: (ShowTxt k, Ord k) => Set k -> T.Text
+fmtSet s = addBrackets $ T.drop 2 $ Set.foldl' (\acc k -> T.concat [acc, ", ", showTxt k]) "" s
 
 -- | Formats a map.
-fmtMap :: (Show k, Show v, Ord k) => Map k v -> String
-fmtMap = Map.foldWithKey (\k v acc -> show k ++ " -> " ++ show v ++ "\n" ++ acc) ""
+fmtMap :: (ShowTxt k, ShowTxt v, Ord k) => Map k v -> T.Text
+fmtMap = Map.foldWithKey (\k v acc -> T.concat [showTxt k, " -> ", showTxt v, "\n", acc]) ""
 
 -- | Formats a map of sets to something
-fmtMapOfSet :: (Show k, Show v, Ord k) => Map (Set k) v -> String
-fmtMapOfSet = Map.foldWithKey (\k v acc -> fmtSet k ++ " -> " ++ show v ++ "\n" ++ acc) ""
+fmtMapOfSet :: (ShowTxt k, ShowTxt v, Ord k) => Map (Set k) v -> T.Text
+fmtMapOfSet = Map.foldWithKey (\k v acc -> T.concat [fmtSet k, " -> ", showTxt v, "\n", acc]) ""
 
 -- | Formats a map of sets (often used to represent undirected networks).
-fmtMapSet :: (Show k0, Show k1, Ord k0, Ord k1) => Map k0 (Set k1) -> String
-fmtMapSet = Map.foldWithKey (\k v acc -> show k ++ " -> " ++ fmtSet v ++ "\n" ++ acc) ""
+fmtMapSet :: (ShowTxt k0, ShowTxt k1, Ord k0, Ord k1) => Map k0 (Set k1) -> T.Text
+fmtMapSet = Map.foldWithKey (\k v acc -> T.concat [showTxt k, " -> ", fmtSet v, "\n", acc]) ""
 
 -- | Formats a map of sets in GraphML format.
-fmtGraphML :: (Show k0, Show k1, Ord k0, Ord k1) => String -> Map k0 (Set k1) -> String
-fmtGraphML name m = hd ++ gr ++ nodes ++ allEdges ++ end
+fmtGraphML :: (ShowTxt k0, ShowTxt k1, Ord k0, Ord k1) => T.Text -> Map k0 (Set k1) -> T.Text
+fmtGraphML name m = T.concat [hd, gr, nodes, allEdges, end]
   where
-    hd = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" ++
-          "<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\"\n" ++
-          " xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" ++
-          " xsi:schemaLocation=\"http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd\">\n"
-    gr = "  <graph id=\"" ++ name ++ "\" edgedefault=\"directed\">\n"
-    nodes = Map.foldWithKey (\k _ acc -> "    <node id=\"" ++ show' k ++ "\"/>\n" ++ acc) "" m
-    allEdges = Map.foldWithKey (\k v acc -> edges k v ++ acc) "" m
-    edges from = Set.fold (\to acc -> "    <edge source=\"" ++ show' from ++ "\" target=\"" ++ show' to ++ "\"/>\n" ++ acc) ""
+    hd = T.concat
+          ["<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+          , "<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\"\n"
+          , " xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n"
+          , " xsi:schemaLocation=\"http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd\">\n"]
+    gr = T.concat ["  <graph id=\"", name, "\" edgedefault=\"directed\">\n"]
+    nodes = Map.foldWithKey (\k _ acc -> T.concat ["    <node id=\"", show' k, "\"/>\n", acc]) "" m
+    allEdges = Map.foldWithKey (\k v acc -> T.concat [edges k v, acc]) "" m
+    edges from = Set.fold (\to acc -> T.concat ["    <edge source=\"", show' from, "\" target=\"", show' to, "\"/>\n", acc]) ""
     end = "  </graph>\n</graphml>\n"
     -- To prevent issues with show String.
-    show' s = rmQuotes $ show s
+    show' s = rmQuotes $ showTxt s
 
 -- | Formats a map of maps (often used to represent networks.)
 fmtMapMap :: (Show k0, Show k1, Show v, Ord k0, Ord k1) => Map k0 (Map k1 v) -> String

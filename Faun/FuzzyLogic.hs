12fb6adfa982bfa992ea8a424aa8039d0df13943
Philippe Desjardins-Proulx <philippe.d.proulx@gmail.com>
2015-12-17 14:44:12 -0500

    finish FuzzyLogic.resolve (must add tests)

--- Faun/FuzzyLogic.hs
+++ Faun/FuzzyLogic.hs
@@ -16,14 +16,18 @@ type FuzzyLogic = Formula FS.FuzzySet
 fuzzyFm :: FuzzyLogic
 fuzzyFm = BinOp And a (BinOp Or b c)
   where
-    a = Atom $ FS.FuzzySet $ Map.fromList [("a", 0.5), ("d", 0.5), ("c", 0.8), ("e", 0.2), ("z", 1.0)]
-    b = Atom $ FS.FuzzySet $ Map.fromList [("x", 0.5), ("a", 0.1), ("d", 0.8), ("y", 0.2)]
+    a = Atom $ FS.FuzzySet $ Map.fromList [("a", 0.5), ("b", 0.5), ("c", 0.8), ("e", 0.2), ("z", 1.0)]
+    b = Atom $ FS.FuzzySet $ Map.fromList [("x", 0.5), ("a", 0.1), ("d", 0.8), ("y", 0.2), ("h", 0.4)]
     c = Atom $ FS.FuzzySet $ Map.fromList [("a", 0.1), ("d", 0.1), ("h", 0.8)]
 
 -- | Resolve the fuzzy system.
 resolve :: FuzzyLogic -> FS.FuzzySet
-resolve f = case f of
-  Atom s          -> s
-  BinOp Or x y    -> FS.intersection (resolve x) (resolve y)
-  BinOp And x y   -> FS.union (resolve x) (resolve y)
-  _               -> FS.FuzzySet Map.empty
+resolve f = let f' = coreOp f in
+  case f' of
+    Atom s          -> s
+    Not x           -> FS.complement (resolve x)
+    BinOp Or x y    -> FS.intersection (resolve x) (resolve y)
+    BinOp And x y   -> FS.union (resolve x) (resolve y)
+    BinOp _ _ _     -> error "Only core operations (or, and) should be left."
+    _               -> FS.FuzzySet Map.empty
+
